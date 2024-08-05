151e356c0b49df84eb4b3766612824d1e595e8cb
Ionut Alexandru Popescu <pialex2032@gmail.com>
2024-08-05 19:18:56 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
     console_error_panic_hook::set_once();
     let mut emulator = emu_lib::emulator::Emulator::new(emu_lib::cpu::CPUType::Z80);
     let rom_data = include_bytes!("../deps/rs_emu_lib/emu_cli/roms/rom.z80.bin");
+    emulator.memory.lock().unwrap().load(rom_data).unwrap();
     println!("Loading rom");
-    emulator.memory.lock().unwrap().load(rom_data).expect("Error loading rom");
     let (mem_in,mem_out) = create_signal(emulator.memory.clone());
 
     mount_to_body(move || view! {

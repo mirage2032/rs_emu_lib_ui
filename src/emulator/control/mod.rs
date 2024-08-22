67531dbebe624193e1bcd725884c0960353cf0cc
Ionut Alexandru Popescu <pialex2032@gmail.com>
2024-08-22 16:06:51 +0300

    Update

--- src/emulator/control/mod.rs
+++ src/emulator/control/mod.rs
@@ -1,9 +1,19 @@
 use emu_lib::emulator::Emulator;
+use leptos::wasm_bindgen::closure::Closure;
+use leptos::wasm_bindgen::JsCast;
 use leptos::*;
 use stylance::import_style;
+use web_sys::js_sys;
+
 import_style!(style, "../table.module.scss");
 #[component]
 pub fn Control(emu_read: ReadSignal<Emulator>, emu_write: WriteSignal<Emulator>) -> impl IntoView {
+    let halted = move || emu_read.with(|emu| emu.cpu.halted());
+    let switch_halt = move || {
+        emu_write.update(|emu| {
+            emu.cpu.set_halted(!emu.cpu.halted());
+        });
+    };
     view! {
         <table class=style::table>
             <tr>
@@ -23,6 +33,60 @@ pub fn Control(emu_read: ReadSignal<Emulator>, emu_write: WriteSignal<Emulator>)
                 </th>
                 <th class=style::tablebutton style:padding="0.3rem">
                     Run
+                </th>
+                <th
+                    style:padding="0.3rem"
+                    class=move || {
+                        if halted() { style::tablebuttoninvert } else { style::tablebutton }
+                    }
+                    on:click=move |_| {
+                        switch_halt();
+                    }
+                >
+                    Halted
+                </th>
+                <th class=style::tablebutton>
+                    <input
+                        on:change=move |event| {
+                            event
+                                .target()
+                                .map(|target| {
+                                    let element = target
+                                        .dyn_into::<web_sys::HtmlInputElement>()
+                                        .unwrap();
+                                    if let Some(files) = element.files() {
+                                        if let Some(file) = files.get(0) {
+                                            let reader = web_sys::FileReader::new().unwrap();
+                                            let reader_clone = reader.clone();
+                                            let onloadend_callback = Closure::wrap(
+                                                Box::new(move || {
+                                                    let array_buffer = reader_clone.result().unwrap();
+                                                    let uint8_array = js_sys::Uint8Array::new(&array_buffer);
+                                                    let file_content = uint8_array.to_vec();
+                                                    emu_write
+                                                        .update(|emu| {
+                                                            emu.memory.load(&file_content).unwrap();
+                                                        });
+                                                    log::info!("{:?}", file_content);
+                                                }) as Box<dyn FnMut()>,
+                                            );
+                                            reader
+                                                .set_onloadend(
+                                                    Some(onloadend_callback.as_ref().unchecked_ref()),
+                                                );
+                                            reader.read_as_array_buffer(&file).unwrap();
+                                            onloadend_callback.forget();
+                                        }
+                                    }
+                                });
+                        }
+                        value="Load file"
+                        type="file"
+                        style:display="none"
+                        id="file-input"
+                    />
+                    <label for="file-input" style:padding="0.3rem">
+                        Load file</label>
                 </th>
             </tr>
         </table>
